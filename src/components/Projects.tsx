0f31d297f62669da4274d6a891e5bd817b69e6d2
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-10 01:25:23 +0700

    update project links

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,10 +13,10 @@ const Projects = () => {
                         <img src="/project-image/sea-salon.png" alt="SEA Salon" className="w-full h-48 object-cover" />
                         <div className="p-6 flex flex-col flex-grow">
                             <h3 className="text-xl font-semibold text-black mb-2">SEA Salon</h3>
-                            <p className="text-gray-600 mb-4">This project is a web-based reservation system for SEA Salon, with features such as reservation, customer reviews, and admin dashboard. I built it with HTML, CSS, JavaScript, and also Firebase for authentication</p>
+                            <p className="text-gray-600 mb-4">This project is a web-based reservation system for SEA Salon, with features such as reservation, customer reviews, and admin dashboard. I built it with HTML, CSS, JavaScript, and also Firebase for authentication.</p>
                             <div className="flex mt-auto justify-between">
-                                <a href="#" className="px-4 py-2 bg-[#3B82F6] text-white rounded hover:bg-blue-600 transition duration-200">Live Demo</a>
-                                <a href="#" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
+                                <a href="https://sea-salon-bice.vercel.app/" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-[#3B82F6] text-white rounded hover:bg-blue-600 transition duration-200">Live Demo</a>
+                                <a href="https://github.com/Fozzzzy/SEA-Salon" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
                             </div>
                         </div>
                     </div>
@@ -26,10 +26,10 @@ const Projects = () => {
                         <img src="/project-image/movie-search-app.png" alt="Movie Search App" className="w-full h-48 object-cover" />
                         <div className="p-6 flex flex-col flex-grow">
                             <h3 className="text-xl font-semibold text-black mb-2">Movie Search App</h3>
-                            <p className="text-gray-600 mb-4">A react project used for searching movies and managing your favorite movie list. I built this using the OMDB API to fetch movie data and allows users to add or remove movies from their favorites list.</p>
+                            <p className="text-gray-600 mb-4">A React project used for searching movies and managing your favorite movie list. I built this using the OMDB API to fetch movie data and allow users to add or remove movies from their favorites list.</p>
                             <div className="flex mt-auto justify-between">
-                                <a href="#" className="px-4 py-2 bg-[#3B82F6] text-white rounded hover:bg-blue-600 transition duration-200">Live Demo</a>
-                                <a href="#" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
+                                <a target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-[#adccff] text-white cursor-pointer rounded hover:bg-blue-600 transition duration-200">Live Demo</a>
+                                <a href="https://github.com/Fozzzzy/react-movie-list-app" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
                             </div>
                         </div>
                     </div>
@@ -39,10 +39,10 @@ const Projects = () => {
                         <img src="/project-image/todo-app.png" alt="Todo List App" className="w-full h-48 object-cover" />
                         <div className="p-6 flex flex-col flex-grow">
                             <h3 className="text-xl font-semibold text-black mb-2">Todo List App</h3>
-                            <p className="text-gray-600 mb-4">This project is used to organize and plan todos for productivity. I built this as my first project to part of learning HTML, CSS, and Javascript.</p>
+                            <p className="text-gray-600 mb-4">This project is used to organize and plan todos for productivity. I built this as my first project as part of learning HTML, CSS, and JavaScript.</p>
                             <div className="flex mt-auto justify-between">
-                                <a href="#" className="px-4 py-2 bg-[#3B82F6] text-white rounded hover:bg-blue-600 transition duration-200">Live Demo</a>
-                                <a href="#" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
+                                <a href="https://todolist-zeta-drab.vercel.app/" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-[#3B82F6] text-white rounded hover:bg-blue-600 transition duration-200">Live Demo</a>
+                                <a href="https://github.com/Fozzzzy/todolist" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
                             </div>
                         </div>
                     </div>
@@ -55,7 +55,7 @@ const Projects = () => {
                             <p className="text-gray-600 mb-4">In Progress . . .  (Coming soon)</p>
                             <div className="flex mt-auto justify-between">
                                 <a href="#projects" className="px-4 py-2 bg-gray-400 text-white rounded cursor-not-allowed">Live Demo</a>
-                                <a href="https://github.com/Fozzzzy/whispy-chat-app.git" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
+                                <a href="https://github.com/Fozzzzy/whispy-chat-app" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition duration-200">GitHub</a>
                             </div>
                         </div>
                     </div>
