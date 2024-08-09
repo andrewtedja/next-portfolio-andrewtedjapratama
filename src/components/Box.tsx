6cf2e693f29f2d8cbc80fbad3996811693890db6
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-10 01:31:13 +0700

    box not showing image bug fixed

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 const Box = () => {
     return (
         <div className="hero-box">
-            <img src="/profile2.png" className="w-full h-full object-cover" loading="lazy"
-                srcSet="/profile2-320w.png 320w, /profile2-480w.png 480w, /profile2-800w.png 800w"
-                sizes="(max-width: 600px) 100vw, 50vw"
-            />
         </div>
-    );
-};
+    )
+}
 
-export default Box;
+export default Box
